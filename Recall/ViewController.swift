75e1c7cfb53d3d7cb5a3d62b5b9069fb14a08b2c
Nicholas Stamoulis <nikolaos11@hotmail.com>
2024-03-20 18:31:30 +0200

    Initial Commit

---
+++ Recall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Recall
+//
+//  Created by stamoulis nikolaos on 20/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
